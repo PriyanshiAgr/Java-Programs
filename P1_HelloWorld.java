33c85731eef915a6c7e7a78fb43dfd4178a5ec53
Priyanshi Agrawal <96936220+PriyanshiAgr@users.noreply.github.com>
2024-03-31 23:17:16 +0530

    Delete P1_HelloWorld.java

--- P1_HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class P1_HelloWorld {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println(name);
-    }
-}
